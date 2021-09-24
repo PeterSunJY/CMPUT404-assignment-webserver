8b1fffd9b37f94990518b18189787d5776b3c1c6
PeterSunJY <mrsunjiayuan@gmail.com>
2021-09-24 15:35:39 -0600

    assignment1

--- README.md
+++ README.md
@@ -17,10 +17,18 @@ server.py contains contributions from:
 * Abram Hindle
 * Eddie Antonio Santos
 * Jackson Z Chang
-* Mandy Meindersma 
+* Mandy Meindersma
+* Jiayuan Sun 
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
+
+References Links
+================
+https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
+https://www.programiz.com/python-programming/datetime/current-datetime
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+https://docs.python.org/3/library/urllib.parse.html
 
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 #  coding: utf-8 
 import socketserver
+import socket
+import os
+from datetime import datetime
+import urllib.parse
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013-2021 Abram Hindle, Eddie Antonio Santos, Jiayuan Sun
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,9 +34,111 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # Get the date and time
+        now = datetime.now()
+        now = now.strftime("%d/%m/%Y %H:%M:%S")
+        self.request.settimeout(0.02)
+        self.getDataBytes = b""
+        while True:
+            try:
+                self.dataTemp = self.request.recv(4096)
+            except:
+                break
+            self.getDataBytes += self.dataTemp
+        # print ("Got a request of: %s\n" % self.getDataBytes)
+        self.getDataList = self.getDataBytes.decode().split()
+        self.method = self.getDataList[0]
+        # If the method is "Get" then proceed, if the method is anything else
+        # response 405 Method Not Allowed
+        if self.method == "GET":
+            self.path = self.getDataList[self.getDataList.index('GET') + 1]
+            self.path = "www" + self.path
+            self.path = urllib.parse.unquote(self.path)
+            if self.path[-1] == "/":
+                self.path += "index.html"
+            try:
+                filename, file_extension = os.path.splitext(self.path)
+                # Users don't specify the file name, the program will add it
+                # and try to open the file. If succeed, response 301 Moved 
+                # Permanently and redirect the page. If fail, response 404 
+                # Not Found
+                if file_extension == "":
+                    try:
+                        tempPath = self.path + "/index.html"
+                        f = open(tempPath, "r")
+                        content = "<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">"
+                        content += "<TITLE>301 Moved Permanently</TITLE></HEAD><BODY>"
+                        content += "<H1>301 Moved Permanently</H1>"
+                        content += "The document has moved"
+                        content += "<A HREF=\"http://127.0.0.1:8080/deep/\"> here</A>."
+                        content += "</BODY></HTML>"
+                        contentLength = str(len(content))
+                        self.data = "HTTP/1.1 301 Moved Permanently\r\n"
+                        self.data += "Location: http://127.0.0.1:8080/deep/\r\n"
+                        self.data += "Date: " + now + "\r\n"
+                        self.data += "Content-Length: " + contentLength + "\r\n"
+                        self.data += "Connection: close\r\n"
+                        self.data += "Content-Type: text/html; charset=UTF-8\r\n"
+                        self.data += "\r\n"
+                        self.data += content + "\r\n\r\n"
+                        self.request.sendall(self.data.encode())
+                        self.request.shutdown(socket.SHUT_WR)
+                    except:
+                        content = "<html><body><h1>404 Not Found</h1></body><html>"
+                        contentLength = str(len(content))
+                        self.data = "HTTP/1.1 404 Not Found\r\n"
+                        self.data += "Date: " + now + "\r\n"
+                        self.data += "Content-Length: " + contentLength + "\r\n"
+                        self.data += "Connection: close\r\n"
+                        self.data += "Content-Type: text/html; charset=UTF-8\r\n"
+                        self.data += "\r\n"
+                        self.data += content + "\r\n\r\n"
+                        self.request.sendall(self.data.encode())
+                        self.request.shutdown(socket.SHUT_WR)
+                # Users specify the file name, try to open the file. If succeed, 
+                # response 200 OK, if fail, response 404 Not Found    
+                else:
+                    f = open(self.path, "r")
+                    content = f.read()
+                    contentLength = str(len(content))
+                    self.data = "HTTP/1.1 200 OK\r\n"
+                    self.data += "Date: " + now + "\r\n"
+                    self.data += "Content-Length: " + contentLength + "\r\n"
+                    self.data += "Connection: Keep-Alive\r\n"
+                    if file_extension == ".html":
+                        self.data += "Content-Type: text/html; charset=UTF-8\r\n"
+                    elif file_extension == ".css":
+                        self.data += "Content-Type: text/css; charset=UTF-8\r\n"
+                    else:
+                        self.data += "Content-Type: application/octec-stream; charset=UTF-8\r\n"
+                    self.data += "\r\n"
+                    self.data += content + "\r\n\r\n"
+                    self.request.sendall(self.data.encode())
+                    self.request.shutdown(socket.SHUT_WR)
+            except:
+                content = "<html><body><h1>404 Not Found</h1></body><html>"
+                contentLength = str(len(content))
+                self.data = "HTTP/1.1 404 Not Found\r\n"
+                self.data += "Date: " + now + "\r\n"
+                self.data += "Content-Length: " + contentLength + "\r\n"
+                self.data += "Connection: close\r\n"
+                self.data += "Content-Type: text/html; charset=UTF-8\r\n"
+                self.data += "\r\n"
+                self.data += content + "\r\n\r\n"
+                self.request.sendall(self.data.encode())
+                self.request.shutdown(socket.SHUT_WR)
+        else:
+            content = "<html><body><h1>405 Method Not Allowed</h1></body><html>"
+            contentLength = str(len(content))
+            self.data = "HTTP/1.1 405 Method Not Allowed\r\n"
+            self.data += "Date: " + now + "\r\n"
+            self.data += "Content-Length: " + contentLength + "\r\n"
+            self.data += "Connection: close\r\n"
+            self.data += "Content-Type: text/html; charset=UTF-8\r\n"
+            self.data += "\r\n"
+            self.data += content + "\r\n\r\n"
+            self.request.sendall(self.data.encode())
+            self.request.shutdown(socket.SHUT_WR)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -43,4 +149,8 @@ if __name__ == "__main__":
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
+    print("Servr listening on http://127.0.0.1:8080/")
     server.serve_forever()
+
+
+
